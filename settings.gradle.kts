2d1340f6a1bccc9816eddc994c79bee572cd02df
liusu239113 <2391130219@qq.com>
2026-06-30 20:16:39 +0800

    fix all

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
         // TapTap SDK 仓库
         maven { url = uri("https://releases.tap.io/repository/tds-public") }
-        // OAID SDK
-        maven { url = uri("https://jitpack.io") }
-        // Huawei Maven（OAID依赖）
-        maven { url = uri("https://developer.huawei.com/repo/") }
-        // Honor Maven
-        maven { url = uri("https://developer.honor.com/repo/") }
     }
 }
 rootProject.name = "WuxiaCrawler"
