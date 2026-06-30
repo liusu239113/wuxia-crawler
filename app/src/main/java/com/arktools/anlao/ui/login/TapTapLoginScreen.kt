355615b45b1e9cb3d11e261ad3fa6bbab855235e
liusu239113 <2391130219@qq.com>
2026-06-30 21:21:56 +0800

    fix: add imports

--- app/src/main/java/com/arktools/anlao/ui/login/TapTapLoginScreen.kt
+++ app/src/main/java/com/arktools/anlao/ui/login/TapTapLoginScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
